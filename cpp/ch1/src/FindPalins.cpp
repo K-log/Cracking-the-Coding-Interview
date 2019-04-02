f497e0079223ab797d453a5bf0382923f0f1c1cb
k-log <noahb343@gmail.com>
2019-04-02 12:54:56 -0700

    Finished FinPalin

--- cpp/ch1/src/FindPalins.cpp
+++ cpp/ch1/src/FindPalins.cpp
@@ -15,25 +15,12 @@ using namespace std;
 bool IsPalin(string s){ 
     // Remove whitespace
     s.erase(remove(s.begin(), s.end(), ' '), s.end());
-    int j = s.size();
-    if(j % 2 == 0){
-        // Case for even strings
-        // TODO: Fix broken end case
-        for(int i = 0; i < (s.size()/2); ++i){
-            if(s[i] != s[j] && i < j){
-                return false;
-            }
-            --j;
+    int j = s.size()-1;
+    for(int i = 0; i < j; i++){
+        if(s[i] != s[j]){
+            return false;
         }
-    } else {
         j--;
-        // Case for odd strings
-        for(int i = 0; i < j; i++){
-            if(s[i] != s[j]){
-                return false;
-            }
-            j--;
-        }
     }
     return true;
 }
@@ -57,15 +44,9 @@ int main(){
     printf("taco cat: \n");
     FindPalins(tests);
 
-    tests = "aab bcc";
-    printf("aab bcc: \n");
+    tests = "abccba";
+    printf("abccba: \n");
     FindPalins(tests);
 
-    /*
-    const char* t = IsPalin("taco cat") ? "true" : "false";
-    printf("IsPalin odd: %s\n", t);
-    t = IsPalin("abccba") ? "true" : "false";
-    printf("IsPalin even: %s\n", t);
-    */
     return 1; 
 }
